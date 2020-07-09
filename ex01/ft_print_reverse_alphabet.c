3f60f84e8d4c862e1916ff5ca0da3af11f498516
Mecayin Mia <memia@c4r2s5.wethinkcode.co.za>
2020-07-09 09:14:52 +0200

    ex01 done

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+int ft_putchar(char c) {
+	write()
+	}
+
+void ft_print_alphabet(void){
+	int count;
+
+	count - 0;
+	while (count < 26)
+	{
+		ft_putchar('z');
+		cunt++;
+	}
+}
