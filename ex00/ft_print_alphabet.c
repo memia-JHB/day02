647f788b1a4317d2e6f2b6c6d052ab9115af68ac
Mecayin Mia <memia@c4r2s5.wethinkcode.co.za>
2020-07-09 09:05:31 +0200

    ex00 done

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+int ft_putchar(char c) {
+	write()
+	}
+
+void ft_print_alphabet(void){
+	int count;
+
+	count - 0;
+	while (count < 26)
+	{
+		ft_putchar('a');
+		cunt++;
+	}
+}
+
